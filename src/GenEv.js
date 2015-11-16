14e0316675046ee028523e4ed992ed3ad3116593
Yehya Awad <yehyaawad@hotmail.com>
2015-11-15 22:00:11 -0500

    Rename private to gfprivate

--- src/GenEv.js
+++ src/GenEv.js
@@ -6,49 +6,49 @@ var GF = function (GENE_STRUCTURE, options) {
     if ((typeof GENE_STRUCTURE === "undefined")) {
         console.error("Invalid parameter passed to GF. You need to pass a gene structure.");
         return null;
-    };
+    }
 
-    // Class Private methods/properties
-    var private = {};
+    // Class private methods/properties
+    var gfprivate = {};
 
     // Gene structure
-    private.GENE_STRUCTURE = GENE_STRUCTURE; // Gene structure
+    gfprivate.GENE_STRUCTURE = GENE_STRUCTURE; // Gene structure
 
     // Maximum population size
-    private.MAX_POPULATION_SIZE = 45; // default to 45
+    gfprivate.MAX_POPULATION_SIZE = 45; // default to 45
 
     // Maximum number of generations
-    private.MAX_GENERATIONS = 100; // default to 100
+    gfprivate.MAX_GENERATIONS = 100; // default to 100
 
     // Mutation probability
-    private.MUTATION_PROB = 0.05;
+    gfprivate.MUTATION_PROB = 0.05;
     c // default to 5%
 
     // Selection number
-    private.NUM_TO_SELECT = 10; // default to 10 tributes (they volunteer as tribute)
+    gfprivate.NUM_TO_SELECT = 10; // default to 10 tributes (they volunteer as tribute)
 
     // Chromosome atrributes
-    private.chromosome = {
+    gfprivate.chromosome = {
         genes: {},
         score: 0
     };
 
     // Generation Population Array
-    private.population = [];
+    gfprivate.population = [];
 
     // Tests a single chromosome's fitness and returns the score
-    private.getFitnessScore = function (genes) {
-        var score = private.fitnessFunction(genes);
+    gfprivate.getFitnessScore = function (genes) {
+        var score = gfprivate.fitnessFunction(genes);
         return score;
     };
 
     // Sort (descending) chromosomes based on score
-    private.sortPopulation = function () {
+    gfprivate.sortPopulation = function () {
 
     };
 
     // Crossover Xgene and Ygene and return crossover
-    private.crossover = function (Xgenes, Ygenes) {
+    gfprivate.crossover = function (Xgenes, Ygenes) {
         var crossedGenes = {};
         for (var property in Xgene) {
             if (Xgene.hasOwnProperty(property)) {
@@ -59,19 +59,19 @@ var GF = function (GENE_STRUCTURE, options) {
     };
 
     // Mutate individual chromosomes
-    private.getMutated = function (genes) {
+    gfprivate.getMutated = function (genes) {
         var mutatedGene = {};
         for (var property in genes) {
             if (genes.hasOwnProperty(property)) {
-                var mutatedGene[property] = ((Math.random() < private.MUTATION_PROB) ? Math.random() : genes[property]);
+                var mutatedGene[property] = ((Math.random() < gfprivate.MUTATION_PROB) ? Math.random() : genes[property]);
             }
         }
         return mutatedGene;
     };
 
     // Generates a new random chromosome using the structure provided
-    private.generateChromosome = function () {
-        var newChromosome = private.GENE_STRUCTURE;
+    gfprivate.generateChromosome = function () {
+        var newChromosome = gfprivate.GENE_STRUCTURE;
         for (var property in newChromosome) {
             if (newChromosome.hasOwnProperty(property)) {
                 newChromosome[property] = Math.random();
@@ -81,7 +81,7 @@ var GF = function (GENE_STRUCTURE, options) {
     };
 
     // Default fitness function (should be replaced with custom function by framework user)
-    private.fitnessFunction = function (genes) {
+    gfprivate.fitnessFunction = function (genes) {
         var score = 0;
         // Do stuff using the genes
         // Return score
@@ -95,28 +95,28 @@ var GF = function (GENE_STRUCTURE, options) {
 
         /* CREATE RANDOM POPULATION */
 
-        for (i = 0; i <= private.MAX_POPULATION_SIZE; i += 1) {
+        for (i = 0; i <= gfprivate.MAX_POPULATION_SIZE; i += 1) {
             // Create new random chromosome
-            var newChromosome = private.generateChromosome();
+            var newChromosome = gfprivate.generateChromosome();
             // Add it to the population
-            private.population.push(newChromosome);
+            gfprivate.population.push(newChromosome);
         };
     };
 
     public.evolve = function (evolveOptions) {
 
         // Update options again.
-        $.extend(private, evolveOptions);
+        $.extend(gfprivate, evolveOptions);
 
         // The generation counter
-        var generationCount = private.MAX_GENERATIONS;
+        var generationCount = gfprivate.MAX_GENERATIONS;
 
         // Set the fitness function
-        private.fitnessFunction = fitnessFunction;
+        gfprivate.fitnessFunction = fitnessFunction;
 
         // Abort
         // If the population is not initialized
-        if (private.population.empty()) {
+        if (gfprivate.population.empty()) {
             console.error("No population initialized. Hint: Use .init before .evolve");
             return;
         };
@@ -126,21 +126,21 @@ var GF = function (GENE_STRUCTURE, options) {
             /* EVALUATION PHASE */
 
             // Pass all chromosomes to fitness function and get all scores
-            for (i = 0; i <= private.population.length; i += 1) {
+            for (i = 0; i <= gfprivate.population.length; i += 1) {
                 // Get the fitness core
-                var score = private.getFitnessScore(private.population[i].genes);
+                var score = gfprivate.getFitnessScore(gfprivate.population[i].genes);
                 // Set the score property of the chromosome
-                private.population[i].score = score;
+                gfprivate.population[i].score = score;
             };
 
             /* SELECTION PHASE */
 
             // Sort most fit to least fit chromosomes
-            private.sortPopulation();
+            gfprivate.sortPopulation();
             // Select best chromosomes
             var fittestChromosomes = [];
-            for (k = 0; k < private.NUM_TO_SELECT; k += 1) {
-                fittestChromosomes.push(private.population[k]);
+            for (k = 0; k < gfprivate.NUM_TO_SELECT; k += 1) {
+                fittestChromosomes.push(gfprivate.population[k]);
             }
 
             /* CROSSOVER & MUTATION PHASE */
@@ -149,21 +149,21 @@ var GF = function (GENE_STRUCTURE, options) {
             for (j = 0; j < fitnessFunction.length; j += 1) {
                 for (jj = 1; jj < fitnessFunction.length - j; jj += 1) {
                     // 50/50 chance for selecting genes
-                    var crossoverChromosome = private.crossover(private.population[j], private.population[jj]);
+                    var crossoverChromosome = gfprivate.crossover(gfprivate.population[j], gfprivate.population[jj]);
                     crossoverPopulation.push(crossoverChromosome);
                 }
             }
 
             /* ASSIGN CROSS OVER AS NEW POPULATION */
 
-            private.population = crossoverPopulation;
+            gfprivate.population = crossoverPopulation;
 
             /* REPEAT UNTIL LAST GENERATION */
         }
     };
 
     // Options merging (requires Jquery)
-    $.extend(private, options);
+    $.extend(gfprivate, options);
 
     return public;
 };
